ae9f810e635a879b624844bb068fe1387e509760
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2019-08-10 21:39:20 +0530

    Update flower.py

--- flower.py
+++ flower.py
@@ -7,11 +7,11 @@ pd.set_option('precision', 2)
 
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
-import  seaborn as sbn
-import  warnings
+import seaborn as sbn
+import warnings
 warnings.filterwarnings(action='ignore')
 
-hnames = ['sepal length' ,'sepal width' ,'petal length' ,'petal width','class']
+hnames = ['sepal length' ,'sepal width' ,'petal length' ,'petal width','kind']
 df = pd.read_csv('iris.csv',names = hnames)
 
 print("\n\n",df.describe(include='all'))
@@ -20,25 +20,21 @@ print("\n Shape of the data set : ",df.shape)
 
 print("\nCheck the count of null values for each feature :  \n", pd.isnull(df).sum())
 
+print("\n\n", df['kind'].value_counts())
+
+labels = ['Iris-setosa' , 'Iris-versicolor'  , 'Iris-virginica']
+plt.pie(df['kind'].value_counts(),labels=labels , autopct='%.1f%%', explode=(0.05,0.05,0.05) , startangle=90 )
+
+# Another way of plotting a pie chart :-
+# df.kind.value_counts().plot(kind="pie", autopct='%.1f%%', figsize=(5,5))
+
 arr = df.values
 x = np.array(arr[:,0:4])
 y = np.array(arr[:,4])
 
 
-sbn.pairplot(df.dropna(),hue='class')
+sbn.pairplot(df.dropna(),hue='kind')
 plt.show()
-
-# df['class'].unique()
-#
-# df = df.loc[(df['class']!='Iris-setosa') | (df['sepal width']>=2.5)]
-# df.loc[df['class'] == 'Iris-setosa', 'sepal width'].hist()
-#
-# df = df.loc[(df['class'] == 'Iris-versicolor') & (df['sepal length'] < 1.0)]
-#
-# df.loc[(df['class'] == 'Iris-versicolor') & (df['sepal length'] < 1.0), 'sepal length'] *= 100.0
-#
-# df.loc[df['class'] == 'Iris-versicolor', 'sepal length'].hist()
-
 
 
 from sklearn.preprocessing import LabelEncoder
@@ -156,3 +152,26 @@ print("\nFor Perceptron Algo accuracy is: ",acc)
 
 # K-means Algo
 
+from sklearn.cluster import KMeans
+
+error=[]
+for n in range(1,10):
+    kmeans = KMeans(n_clusters=n , random_state= 0)
+    kmeans.fit(x_train)
+    error.append(kmeans.inertia_)
+
+plt.plot(range(1,10) , error)
+plt.scatter(3 , error[2] , c='r' , marker='x',label='Elbow')
+plt.legend()
+plt.title('The elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distortion')
+plt.show()
+
+kmeans = KMeans(n_clusters=3 , random_state=1)
+y_kmeans = kmeans.fit_predict(x_train)
+print(y_kmeans)
+
+plt.scatter(kmeans.cluster_centers_[:,0] , kmeans.cluster_centers_[: , 1] , s=10 , c='red' , label = 'Centroids')
+plt.legend()
+plt.show()
